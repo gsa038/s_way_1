46f49b943efcb378995c5799326d93928a2df6fe
gsa038@ya.ru <gsa038@ya.ru>
2020-03-26 17:27:12 +0300

    1

--- src/api/api.js
+++ src/api/api.js
@@ -10,7 +10,7 @@ const instance = axios.create({
 
 export const usersAPI = {
 
-    getUsers(currentPage = 1, pageSize = 100) {
+    getUsers(currentPage, pageSize) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data)
     },
