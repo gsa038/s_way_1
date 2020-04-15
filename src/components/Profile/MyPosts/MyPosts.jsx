5ae957d09a28c28137ec0627fe83bb1f63921c6d
Stanislav Gnoyanko <gsa038@gmail.com>
2020-04-15 21:59:26 +0300

    87 MyPosts as functional component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,36 +8,21 @@ import { InputArea } from '../../common/FormsControls/FormsControls';
 const maxlength10 = maxlength(10);
 const textArea = InputArea("textarea")
 
-class MyPosts extends React.PureComponent {
-
-    componentDidMount() {
-        setTimeout( () => {
-            this.setState({a: 15})
-        }, 5000)
-    }
-
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     return nextProps !== this.props || nextState !== this.state
-    // }
-
-    render() {
-        console.log('render MyPosts')
-        let postsElements = this.props.posts.map( p => <Post message={p.message} key={p.id} likesCounts={p.likesCounts}/>);
-
-        let addPost = (values) => {
-            this.props.addPost(values.newPostText);
-        };
-
-        return (
-            <div className={s.posts_block}>
-                <h3>My posts</h3>
-                <AddPostReduxForm onSubmit={addPost}/>
-                <div className={s.posts}>
-                    { postsElements }
-                </div >
-            </div>
-        );
-    }
+const MyPosts = props => {
+    console.log('render MyPosts')
+    let postsElements = props.posts.map( p => <Post message={p.message} key={p.id} likesCounts={p.likesCounts}/>);
+    let addPost = (values) => {
+        props.addPost(values.newPostText);
+    };
+    return (
+        <div className={s.posts_block}>
+            <h3>My posts</h3>
+            <AddPostReduxForm onSubmit={addPost}/>
+            <div className={s.posts}>
+                { postsElements }
+            </div >
+        </div>
+    );
 }
 
 const AddPostForm = (props) => {
