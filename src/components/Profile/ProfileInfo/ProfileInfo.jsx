740436049543b906524dd5150934f06258cf68a7
gsa038@ya.ru <gsa038@ya.ru>
2020-04-23 16:49:45 +0300

    updPfofileInfo_work_230420

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,49 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
 import defaultUserPhoto from '../../../assets/images/236832.png';
 // import ProfileStatus from './ProfileStatus';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import cn from 'classnames';
 
 const ProfileInfo = ({ isOwner, userProfile, status, updateStatus, uploadPhoto }) => {
+    
+    let [editMode, setEditMode] = useState(false);
+    const deactivateEditMode = () => {
+        setEditMode(false);
+    }
     if (!userProfile) {
         return <Preloader />
     }
 
     const onMainPhotoSelected = (e) => {
-        if (e.target.files.length)
-        {
+        if (e.target.files.length) {
             uploadPhoto(e.target.files[0]);
         }
     }
-    debugger;
     return (
         <div>
-            {/* <div className={s.profile_img}>
-                <img src="https://wallpaperaccess.com/full/121194.jpg" alt="Profile"></img>
-            </div> */}
-            <div className={s.description_block}>
-                <div className={s.ava_img}>
-                    <img src={userProfile.photos.large || defaultUserPhoto} alt="Avatar"></img>
-                    {isOwner && <div>
-                        <input type="file" onChange={onMainPhotoSelected} />
-                    </div>}
-                    <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
-                    <div>{'ID: ' + userProfile.userId}</div>
+            <div className={s.description}>
+                <div>
+                    <div className={s.ava_img}>
+                        <img src={userProfile.photos.large || defaultUserPhoto} alt="Avatar"></img>
+                        {isOwner && <div>
+                            <input type="file" onChange={onMainPhotoSelected} />
+                        </div>}
+                    </div>
+                    <div>
+                        <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
+                    </div>
                 </div>
-                <div className="">
-                    <div>FullName: {userProfile.fullName}</div>
-                    <div>About me:</div>
-                    <div>{userProfile.aboutMe}</div>
-                    <div>Contacts:</div>
-                    { 
-                        Object.entries(userProfile.contacts).map(([k, v]) => v ? <div>{k}: {v}</div>: null) 
-                    }
-                    <div>Looking for a job: {userProfile.lookingForAJob ? "Yes" : "No"}</div>
-                </div>
+                { editMode 
+                    ? <ProfileDataForm userProfile={userProfile} /> 
+                    : <ProfileData 
+                        userProfile={userProfile} 
+                        isOwner={isOwner} 
+                        goToEditMode={ () => {setEditMode(true)} } 
+                        deactivateEditMode={ deactivateEditMode } /> }
             </div>
         </div>
     );
+}
+
+const ProfileData = ({userProfile, isOwner, goToEditMode}) => {
+    return (
+    <div>
+        {isOwner && <div className><button className={s.editDescriptionButton} onClick={goToEditMode}>edit</button></div>}
+        <div className={s.descriptionItem}><b>Fullname:</b> {userProfile.fullName}</div>
+        <div className={s.descriptionItem}><b>About me:</b></div>
+        <div className={s.descriptionItem}>{userProfile.aboutMe}</div>
+        <div className={s.descriptionItem}><b>Contacts:</b></div>
+        <div className={cn(s.contactsChilds, s.descriptionItem)}>{
+            Object.entries(userProfile.contacts).map(([k, v]) => v ? <div><b>{k}</b>: {v}</div> : null)
+        }</div>
+        <div><b>Looking for a job:</b> {userProfile.lookingForAJob ? "Yes" : "No"}</div>
+        {userProfile.lookingForAJob && <div><b>My professionals skills:</b> userProfile.lookingForAJobDescription</div>}
+    </div>
+    )
+}
+const ProfileDataForm = ({userProfile}) => {
+    return (
+    <div className={s.descriptionRightColumn}>
+        Form
+    </div>
+    )
 }
 
 export default ProfileInfo;
